c79a92e56c0fa8af2c3122795755e667bb016300
Chuck Remes <git@chuckremes.com>
2018-01-03 09:41:24 -0600

    refactor and simplify

--- lib/io/platforms/common/poller.rb
+++ lib/io/platforms/common/poller.rb
@@ -10,15 +10,16 @@ class IO
     class SelectPoller
       MAX_EVENTS = 10
       NO_TIMEOUT = TimeSpecStruct.new
-      SHORT_TIMEOUT = TimeSpecStruct.new.tap { |ts| ts[:tv_sec] = 1 }
+      SHORT_TIMEOUT = TimeSpecStruct.new.tap { |ts| ts[:tv_nsec] = 1 }
 
       def initialize
-        @master_read_set = FDSetStruct.new
-        @master_write_set = FDSetStruct.new
+        @read_master_set   = FDSetStruct.new
+        @read_working_set  = FDSetStruct.new
+        @write_master_set  = FDSetStruct.new
+        @write_working_set = FDSetStruct.new
 
         @read_callbacks = {}
         @write_callbacks = {}
-        @timer_callbacks = {}
         Logger.debug(klass: self.class, name: 'select poller', message: 'poller allocated!')
       end
 
@@ -27,71 +28,62 @@ class IO
       end
 
       def register_timer(duration:, request:)
-#        @timer_callbacks[request.object_id] = request
-#        register(
-#          fd: 1,
-#          request: request,
-#          filter: Constants::EVFILT_TIMER,
-#          flags: Constants::EV_ADD | Constants::EV_ENABLE | Constants::EV_ONESHOT,
-#          fflags: Constants::NOTE_MSECONDS,
-#          data: duration,
-#          udata: request.object_id
-#        )
-#        Logger.debug(klass: self.class, name: 'kqueue poller', message: "registered for timer, object_id [#{request.object_id}]")
+        #        @timer_callbacks[request.object_id] = request
+        #        register(
+        #          fd: 1,
+        #          request: request,
+        #          filter: Constants::EVFILT_TIMER,
+        #          flags: Constants::EV_ADD | Constants::EV_ENABLE | Constants::EV_ONESHOT,
+        #          fflags: Constants::NOTE_MSECONDS,
+        #          data: duration,
+        #          udata: request.object_id
+        #        )
+        #        Logger.debug(klass: self.class, name: 'kqueue poller', message: "registered for timer, object_id [#{request.object_id}]")
       end
 
       def register_read(fd:, request:)
         @read_callbacks[fd] = request
-        register(
-          fd: fd,
-          request: request,
-          fdset: @master_read_set
-        )
+        @read_master_set.set(fd: fd)
+
         Logger.debug(klass: self.class, name: 'select poller', message: "registered for read, fd [#{fd}]")
       end
 
       def register_write(fd:, request:)
         @write_callbacks[fd] = request
-        register(
-          fd: fd,
-          request: request,
-          fdset: @master_write_set
-        )
+        @write_master_set.set(fd: fd)
+
         Logger.debug(klass: self.class, name: 'select poller', message: "registered for write, fd [#{fd}]")
       end
 
       # Dispatches the registered reads and writes to +select+.
       def poll
         Logger.debug(klass: self.class, name: 'select poller', message: 'calling select')
-        read_working_set = @master_read_set.max_fd > 0 ? @master_read_set.copy : nil
-        write_working_set = @master_write_set.max_fd > 0 ? @master_write_set.copy : nil
-        max_fd = [@master_read_set.max_fd, @master_write_set.max_fd].max
-        
+        # Copy FDs from master to working set if there are any; else set local working set
+        # to nil so select(2) ignores it. Saves on reallocating working_set every loop.
+        read_working_set = @read_master_set.max_fd >= 0 ? @read_master_set.copy_to(copy: @read_working_set) : nil
+        write_working_set = @write_master_set.max_fd >= 0 ? @write_master_set.copy_to(copy: @write_working_set) : nil
+
+        max_fd = [@read_master_set.max_fd, @write_master_set.max_fd].max
+
         rc = Platforms.select(max_fd + 1, read_working_set, write_working_set, nil, SHORT_TIMEOUT)
         Logger.debug(klass: self.class, name: 'select poller', message: "select returned [#{rc}] events!")
 
         if rc >= 0
-          p read_working_set
-          read_working_set && (read_working_set.max_fd + 1).times do |index|
-            break if 0 == rc
-            puts "checking read fd [#{index}]"
-            if read_working_set.set?(fd: index)
-              @master_read_set.clear(fd: index)
-              process_read_event(event: index)
-              rc -= 1
-            end
-          end
-          
-          p write_working_set
-          write_working_set && (write_working_set.max_fd + 1).times do |index|
-            break if 0 == rc
-            puts "checking write fd [#{index}]"
-            if write_working_set.set?(fd: index)
-              @master_write_set.clear(fd: index)
-              process_write_event(event: index)
-              rc -= 1
-            end
-          end
+          process_events(
+            count: rc,
+            working_set: read_working_set,
+            master_set: @read_master_set,
+            callbacks: @read_callbacks,
+            kind: 'READ'
+          )
+
+          process_events(
+            count: rc,
+            working_set: write_working_set,
+            master_set: @write_master_set,
+            callbacks: @write_callbacks,
+            kind: 'WRITE'
+          )
         else
           Logger.debug(klass: self.class, name: 'select poller', message: "rc [#{rc}], errno [#{::FFI.errno}]")
         end
@@ -99,12 +91,19 @@ class IO
 
       private
 
-      def process_read_event(event:)
-        execute_callback(identity: event, callbacks: @read_callbacks, kind: 'READ')
-      end
+      def process_events(count:, working_set:, master_set:, callbacks:, kind:)
+        Logger.debug(klass: self.class, name: 'select poller', message: "max_fd [#{master_set.max_fd}]")
+        return if master_set.max_fd < 0
 
-      def process_write_event(event:)
-        execute_callback(identity: event, callbacks: @write_callbacks, kind: 'WRITE')
+        master_set.each do |fd|
+          break if 0 == count
+          puts "checking #{kind} fd [#{fd}]"
+          if working_set.set?(fd: fd)
+            master_set.clear(fd: fd)
+            execute_callback(identity: fd, callbacks: callbacks, kind: kind)
+            count -= 1
+          end
+        end
       end
 
       def execute_callback(identity:, callbacks:, kind:)
@@ -115,10 +114,6 @@ class IO
         else
           raise "Got [#{kind}] event for fd [#{identity}] with no registered callback"
         end
-      end
-
-      def register(fd:, request:, fdset:)
-        fdset.set(fd: fd)
       end
     end
 
