fbf484aec0b8b1baee6bf61252da72d353481874
Chuck Remes <git@chuckremes.com>
2018-01-05 16:15:28 -0600

    fix buffered reads and document; fix timeout arg

--- lib/io/mixins/enumerable.rb
+++ lib/io/mixins/enumerable.rb
@@ -40,13 +40,13 @@ class IO
               io: @io,
               limit: @limit,
               offset: offset,
-              timeout: timeout
+              timeout: @timeout
             )
 
             if rc < 0
               yield(nil, rc, errno)
             else
-              yield(buffer.read_bytes(rc), rc, nil)
+              yield(buffer, rc, nil)
               offset += rc
             end
           end until rc.zero? # end of file
@@ -66,12 +66,12 @@ class IO
           if rc < 0
             [nil, rc, errno]
           else
-            [rc, nil, @read_buffer.get_bytes(0, limit)]
+            [rc, nil, @read_buffer.get_bytes(0, rc)]
           end
         end
       end
 
-      def each(limit:, offset: 0, timeout:, &blk)
+      def each(limit:, offset: 0, timeout: nil, &blk)
         # fail silently for now. need to hook up error policy here.
         #raise LimitArgError.new(limit) if limit < 1
         #raise OffsetArgError.new(offset) if offset < 0
@@ -79,7 +79,7 @@ class IO
         self
       end
 
-      def each_byte(offset:, timeout:)
+      def each_byte(offset:, timeout: nil)
         each(limit: 1, offset: offset, timeout: timeout) do |byte, rc, errno|
           yield(byte, rc, errno)
         end
@@ -114,42 +114,60 @@ class IO
 
         def initialize(io:, limit:, offset:, timeout:)
           super
-          @saved_buffer = ''.force_encoding(Encoding::ASCII_8BIT)
+          @saved_buffer = String.new # always returns encoding ASCII_8BIT
+          @read_buffer  = ::FFI::MemoryPointer.new(READ_SIZE)
         end
 
         def read_from_storage(io:, limit:, offset:, timeout:)
           if limit > @saved_buffer.size
             # we cannot satisfy request from saved bytes
-            # so read another full buffer from FD
+            # so try to read another full buffer from FD
             rc, errno, buffer = io.read(
               nbytes: READ_SIZE,
-              offset: offset,
+              offset: offset + @saved_buffer.size, # move offset past remaining buffer
               buffer: @read_buffer,
               timeout: timeout
             )
 
             if rc < 0
+              # failure
               [nil, rc, errno]
             else
-              # add correct number of newly read bytes to saved_buffer
-              # note that read might not return enough to satisfy
-              # entire requested limit
-              bytes_read = @saved_buffer.size + rc
-              string_buffer = @saved_buffer << @read_buffer.get_bytes(0, bytes_read)
+              # @saved_buffer may have data or may be 0 on first time through;
+              # we need the MINIMUM of our limit minus saved buffer OR the
+              # total bytes read just now
+              bytes_needed = [limit - @saved_buffer.size, rc].min
 
-              # make sure to save remainder of buffer
-              @saved_buffer = @read_buffer.get_bytes(bytes_read, READ_SIZE)
+              # prepare string to return to caller by combining any saved
+              # bytes and reading +bytes_needed+ bytes from refilled buffer
+              string_buffer = if bytes_needed > 0
+                # minimize String copying by only doing this append when bytes available
+                @saved_buffer << @read_buffer.get_bytes(0, bytes_needed)
+              else
+                @saved_buffer
+              end
 
-              [bytes_read, nil, string_buffer]
+              # lastly, we may have bytes left in read buffer; save the unused
+              # bytes for a future request; if at EOF, this is 0
+              @saved_buffer = if (rc - bytes_needed) > 0
+                @read_buffer.get_bytes(bytes_needed, rc - bytes_needed)
+              else
+                String.new
+              end
+
+              [string_buffer.size, nil, string_buffer]
             end
           elsif limit <= @saved_buffer.size
+
             # we can satisfy entire request from buffered bytes
-            [limit, nil, @saved_buffer.slice!(0, limit)]
+            # read whichever is smaller, the remaining buffered bytes or +limit+ bytes
+            length = [@saved_buffer.size, limit].min
+            [length, nil, @saved_buffer.slice!(0, length)]
           end
         end
       end
 
-      def each(limit:, offset: 0, timeout:, &blk)
+      def each(limit:, offset: 0, timeout: nil, &blk)
         # fail silently for now. need to hook up error policy here.
         #raise LimitArgError.new(limit) if limit < 1
         #raise OffsetArgError.new(offset) if offset < 0
@@ -157,7 +175,7 @@ class IO
         self
       end
 
-      def each_byte(offset:, timeout:)
+      def each_byte(offset:, timeout: nil)
         each(limit: 1, offset: offset, timeout: timeout) do |byte, rc, errno|
           yield(byte, rc, errno)
         end
