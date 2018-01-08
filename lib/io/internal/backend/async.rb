0685d23ca37b9050c473854887a79930be45e3ea
Chuck Remes <git@chuckremes.com>
2018-01-08 16:01:13 -0600

    remove some superfluous code to clean things up

--- lib/io/internal/backend/async.rb
+++ lib/io/internal/backend/async.rb
@@ -6,168 +6,123 @@ class IO
       class Async
         class << self
           def fcntl(fd:, cmd:, args:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.fcntl(fd, cmd, args)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def open(path:, flags:, mode:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.open(path, flags.to_i, mode.to_i)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def close(fd:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.close(fd)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def read(fd:, buffer:, nbytes:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.read(fd, buffer, nbytes)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def write(fd:, buffer:, nbytes:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.write(fd, buffer, nbytes)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def pread(fd:, buffer:, nbytes:, offset:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.pread(fd, buffer, nbytes, offset)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def pwrite(fd:, buffer:, nbytes:, offset:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.pwrite(fd, buffer, nbytes, offset)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def getaddrinfo(hostname:, service:, hints:, results:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.getaddrinfo(hostname, service, hints, results)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def socket(domain:, type:, protocol:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.socket(domain, type, protocol)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def bind(fd:, addr:, addrlen:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.bind(fd, addr, addrlen)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def connect(fd:, addr:, addrlen:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.connect(fd, addr, addrlen)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def listen(fd:, backlog:, timeout:)
-            reply = build_blocking_request do |fiber|
+            build_blocking_request do |fiber|
               build_command(fiber) do
                 Platforms::Functions.listen(fd, backlog)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def accept(fd:, addr:, addrlen:, timeout:)
-            reply = build_poll_read_request(fd: fd, repeat: false) do |fiber|
+            build_poll_read_request(fd: fd, repeat: false) do |fiber|
               build_command(fiber) do
                 Platforms::Functions.accept(fd, addr, addrlen)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def ssend(fd:, buffer:, flags:, timeout:)
-            reply = build_poll_write_request(fd: fd, repeat: false) do |fiber|
+            build_poll_write_request(fd: fd, repeat: false) do |fiber|
               build_command(fiber) do
                 Platforms::Functions.ssend(fd, buffer, buffer.size, flags.to_i)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def recv(fd:, buffer:, flags:, timeout:)
-            reply = build_poll_read_request(fd: fd, repeat: false) do |fiber|
+            build_poll_read_request(fd: fd, repeat: false) do |fiber|
               build_command(fiber) do
                 Platforms::Functions.recv(fd, buffer, buffer.size, flags.to_i)
               end
             end
-
-            #Policy.check(reply)
-            reply
           end
 
           def timer(duration:)
--- lib/io/internal/backend/sync.rb
+++ lib/io/internal/backend/sync.rb
@@ -4,108 +4,63 @@ class IO
       class Sync
         class << self
           def fcntl(fd:, cmd:, args:, timeout:)
-            reply = Platforms::Functions.fcntl(fd, cmd, args)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.fcntl(fd, cmd, args)
           end
 
           def open(path:, flags:, mode:, timeout:)
-            reply = Platforms::Functions.open(path, flags.to_i, mode.to_i)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.open(path, flags.to_i, mode.to_i)
           end
 
           def close(fd:, timeout:)
-            reply = Platforms::Functions.close(fd)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.close(fd)
           end
 
           def read(fd:, buffer:, nbytes:, timeout:)
-            reply = Platforms::Functions.read(fd, buffer, nbytes)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.read(fd, buffer, nbytes)
           end
 
           def write(fd:, buffer:, nbytes:, timeout:)
-            reply = Platforms::Functions.write(fd, buffer, nbytes)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.write(fd, buffer, nbytes)
           end
 
           def pread(fd:, buffer:, nbytes:, offset:, timeout:)
-            reply = Platforms::Functions.pread(fd, buffer, nbytes, offset)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.pread(fd, buffer, nbytes, offset)
           end
 
           def pwrite(fd:, buffer:, nbytes:, offset:, timeout:)
-            reply = Platforms::Functions.pwrite(fd, buffer, nbytes, offset)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.pwrite(fd, buffer, nbytes, offset)
           end
 
           def getaddrinfo(hostname:, service:, hints:, results:, timeout:)
-            reply = Platforms::Functions.getaddrinfo(hostname, service, hints, results)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.getaddrinfo(hostname, service, hints, results)
           end
 
           def socket(domain:, type:, protocol:, timeout:)
-            reply = Platforms::Functions.socket(domain, type, protocol)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.socket(domain, type, protocol)
           end
 
           def bind(fd:, addr:, addrlen:, timeout:)
-            reply = Platforms::Functions.bind(fd, addr, addrlen)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.bind(fd, addr, addrlen)
           end
 
           def connect(fd:, addr:, addrlen:, timeout:)
-            reply = Platforms::Functions.connect(fd, addr, addrlen)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.connect(fd, addr, addrlen)
           end
 
           def listen(fd:, backlog:, timeout:)
-            reply = Platforms::Functions.listen(fd, backlog)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.listen(fd, backlog)
           end
 
           def accept(fd:, addr:, addrlen:, timeout:)
-            reply = Platforms::Functions.accept(fd, addr, addrlen)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.accept(fd, addr, addrlen)
           end
 
           def ssend(fd:, buffer:, flags:, timeout:)
-            reply = Platforms::Functions.ssend(fd, buffer, buffer.size, flags)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.ssend(fd, buffer, buffer.size, flags)
           end
 
           def recv(fd:, buffer:, flags:, timeout:)
-            reply = Platforms::Functions.recv(fd, buffer, buffer.size, flags)
-
-            #Policy.check(reply)
-            reply
+            Platforms::Functions.recv(fd, buffer, buffer.size, flags)
           end
         end
       end
