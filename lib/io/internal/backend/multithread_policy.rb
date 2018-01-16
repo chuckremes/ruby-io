8d13359f54146946e210ff23d775b798bec0aaae
Chuck Remes <git@chuckremes.com>
2018-01-15 20:32:57 -0600

    improve log message for multithread policy WARNING

--- lib/io/internal/backend/multithread_policy.rb
+++ lib/io/internal/backend/multithread_policy.rb
@@ -7,24 +7,24 @@ class IO
             true
           end
         end
-        
+
         class Warn
           WARNING = 'WARNING: It is NOT recommened to call IO objects from multiple threads!'
           def self.check(io:, creator:, current: Thread.current)
             return true if creator == current
-            
-            STDERR.puts WARNING
-            string = "IO object #{io.inspect}\n"
-            string += "Created in thread #{creator.inspect}\n"
-            string += "Called by thread #{current.inspect}\n"
+
+            STDERR.puts WARNING +
+              " IO object #{io.inspect}\n" +
+              "Created in thread #{creator.inspect}\n" +
+              "Called by thread #{current.inspect}\n"
             true
           end
         end
-        
+
         class Fatal
           def self.check(io:, creator:, current: Thread.current)
             return true if creator == current
-            
+
             # FIXME: Use correct exception here
             raise "Thread #{creator.inspect} created IO object #{io.inspect} but called from other thread #{current.inspect}"
           ensure
