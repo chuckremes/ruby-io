86109e3222eff28cf680934f2c24b509c72654f6
Chuck Remes <git@chuckremes.com>
2018-01-31 11:33:22 -0600

    remove doodle files

--- a.rb
+++
@@ -1,8 +0,0 @@
-Thread.abort_on_exception = true
-
-t = Thread.new do
-  raise 'Should propogate to main thread'
-end
-
-sleep 1
-
--- z.rb
+++
@@ -1,9 +0,0 @@
-require 'fiber'
-
-begin
-  Fiber.new { Fiber.yield Fiber.current }.transfer.resume
-  puts "Cool I'm in ruby 1.9.x. I've got fibers!"
-rescue FiberError
-  puts "Too bad, I'm in 2.x and no transfer/yield/resume for me. Is it supposed to be a feature? :("
-end
-
