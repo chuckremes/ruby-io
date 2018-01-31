13146885d80df9af58e0a1332e40c8c708d4084e
Chuck Remes <git@chuckremes.com>
2018-01-31 10:42:59 -0600

    clean up a lot of the puts-style debugging

--- examples/echo_tcp.rb
+++ examples/echo_tcp.rb
@@ -19,7 +19,6 @@ Thread.current.local[:name] = 'MAIN' if DEBUG
 
 
 server_thread = IO::Internal::Thread.new do
-  Thread.current.local[:name] = 'SERVER' if DEBUG
   server = IO::TCP.ip4(addrinfo: structs.first)
   addr = structs.first.sock_addr_ref
   server.bind(addr: addr)
@@ -34,50 +33,38 @@ server_thread = IO::Internal::Thread.new do
     if fd > 0
       begin
         i += 1
-        puts "[#{Time.now - server_accept_time}] seconds to accept client-#{i}" if DEBUG
-        server_accept_time = Time.now
-        puts "#{tid}, iteration-#{i}, ACCEPTED A SOCKET, rc [#{fd}], errno [#{errno}]" if DEBUG
         rc, errno, string = socket.recv(buffer: nil, nbytes: 20, flags: 0)
-        puts "#{tid}, iteration-#{i}, SERVER: recv: rc [#{rc}], errno [#{errno}], string [#{string}]" if DEBUG
+        raise "Server failed to receive! rc [#{rc}], errno [#{errno}]" if rc < 0
         puts "Server received message: #{string}"
-
-        puts "#{tid}, iteration-#{i}, SERVER: sending response, [#{string}]" if DEBUG
 
         if string == 'exit'
           server_wait = false
-          puts "Server received exit message, server_wait [#{server_wait}]" if DEBUG
           rc, errno = socket.send(buffer: 'goodbye', nbytes: 'goodbye'.size, flags: 0)
-          puts "SERVER: marking accept loop break"
+          raise "Server failed to send exit message!" if rc < 0
           server.accept_break
         elsif server_wait
           timer_reply = IO::Timer.sleep(seconds: rand(RAND_SLEEP))
-          puts "Server echoing back: #{string}"
+          puts "Server echoing back: #{string}, #{Time.now.to_f}"
           rc, errno = socket.send(buffer: string, nbytes: string.size, flags: 0)
+          raise "Server failed to send echo message: #{string}, rc [#{rc}], errno [#{errno}]" if rc < 0
           server_sent += 1
           server_wait = false if server_sent >= ClientCount - 1
-          puts "#{tid}, iteration-#{i}, SERVER: send: rc [#{rc}], errno [#{errno}], server_sent [#{server_sent}], server_wait [#{server_wait}]" if DEBUG
-          puts "#{tid}, iteration-#{i}, SERVER: closing socket" if DEBUG
-          # +socket+ closes automatically when block exits
         end
       rescue => e
-        puts "GOT A SERVER EXCEPTION:  #{e.inspect}"
+        STDERR.puts "GOT A SERVER EXCEPTION:  #{e.inspect}"
       end
     else
-      puts "SERVER: shutting down early, accepted socket failed."
+      STDERR.puts "SERVER: shutting down early, accepted socket failed."
     end
-    puts "#{tid}, iteration-#{i}, exiting [#{i}] server block, server_wait [#{server_wait}]" if DEBUG
   end
 
-  puts "finalizing server loop..."
   Thread.current.local[:_scheduler_].finalize_loop
-  puts "#{tid}, server thread exiting..." #if DEBUG
 end
 
 completed_count = 0
 sleep 0.2
 
 client_thread = IO::Internal::Thread.new do
-  Thread.current.local[:name] = 'CLIENT' if DEBUG
   clients = []
   times = {}
 
@@ -86,44 +73,40 @@ client_thread = IO::Internal::Thread.new do
     top = Time.now
     client = IO::TCP.ip4(addrinfo: structs.first)
     addr = structs.first.sock_addr_ref
-    puts "#{tid}, CLIENT-#{i}, TRYING TO CONNECT" if DEBUG
-    times[i] = Time.now
+
     client.connect(addr: addr) do |sock, rc, errno|
       begin
-        puts "[#{Time.now - times[i]}] seconds to connect client-#{i}" if DEBUG
         client_sent_time = Time.now
-        puts "#{tid}, CLIENT-#{i}, INSIDE CONNECT BLOCK, rc [#{rc}], errno [#{errno}], sock [#{sock.inspect}]" if DEBUG
+        fd = sock.instance_variable_get(:@fd)
         raise "Client-#{i} failed to connect, rc [#{rc}], errno [#{errno}]" if rc < 0
 
         msg = "echo message: #{i}"
-        puts "#{tid}, CLIENT-#{i}: send reply, msg [#{msg}]" if DEBUG
         puts "Client-#{i} sending message: #{msg}"
         rc, errno = sock.send(buffer: msg, nbytes: msg.size, flags: 0)
-        puts "#{tid}, CLIENT-#{i}: send: rc [#{rc}], errno [#{errno}]" if DEBUG
+        raise "Client-#{i} failed to send! rc [#{rc}], errno [#{errno}]" if rc < 0
         rc, errno, string = sock.recv(buffer: nil, nbytes: 20, flags: 0)
-        puts "#{tid}, CLIENT-#{i}: recv: rc [#{rc}], errno [#{errno}], string [#{string}]" if DEBUG
+        if string.size < 5
+          puts "Client-#{i} got a short response, let's try receiving again, #{Time.now.to_f}"
+          rc, errno, string = sock.recv(buffer: nil, nbytes: 20, flags: 0)
+          puts "Client-#{i} second echo!"
+        end
+        raise "received string [#{string.inspect}] is empty! rc [#{rc}], errno [#{errno}]" if string.size < 5
         puts "Client-#{i} received message: #{string}, roundtrip time [#{Time.now - client_sent_time}] seconds"
-        puts "#{tid}, CLIENT-#{i}, closing socket" if DEBUG
-
-        # +sock+ is automatically closed when block exits
-        #sock.close
         completed_count += 1
+        IO::Timer.sleep(seconds: 8)
       rescue => e
-        puts "GOT A CLIENT EXCEPTION!: #{e.inspect}"
+        STDERR.puts "GOT A CLIENT-#{i} EXCEPTION!: #{e.inspect}"
       end
     end
-    puts "times[#{i}] took: #{Time.now - times[i]} seconds"
   end
-  puts "[#{Time.now - start_connects}] seconds to issue all #{ClientCount} connection requests."
+
   IO::Timer.sleep(seconds: 1) until completed_count >= ClientCount
 
   client = IO::TCP.ip4(addrinfo: structs.first)
   addr = structs.first.sock_addr_ref
-  puts "LAST CLIENT TRYING TO CONNECT" if DEBUG
   can_exit = false
   client.connect(addr: addr) do |sock, rc, errno|
     raise "Client-last failed to connect, rc [#{rc}], errno [#{errno}]" if rc < 0
-    puts "Last client telling Server to exit..."
     rc, errno = sock.send(buffer: 'exit', nbytes: 'exit'.size, flags: 0)
     raise "Client-last failed to send message, rc [#{rc}], errno [#{errno}]" if rc < 0
     completed_count += 1
@@ -135,16 +118,12 @@ client_thread = IO::Internal::Thread.new do
 
   IO::Timer.sleep(seconds: 1) until can_exit
 
-  puts "finalizing client loop..."
   Thread.current.local[:_scheduler_].finalize_loop
-  puts "#{tid}, client completed count [#{completed_count}]" if DEBUG
-  puts "#{tid}, client thread exiting..." if DEBUG
 end
 
 #threads << server_thread
 threads << client_thread
 
 threads.each { |thr| thr.join }
-#server_thread.kill
 
 puts 'echo.rb is exiting normally... all threads joined.'
