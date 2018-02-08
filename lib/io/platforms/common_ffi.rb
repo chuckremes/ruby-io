db2a8ee2e000381479de9180c867dfd677d94f03
Chuck Remes <git@chuckremes.com>
2018-02-08 10:46:32 -0600

    force usage of #select on all platforms temporarily

--- lib/io/platforms/common_ffi.rb
+++ lib/io/platforms/common_ffi.rb
@@ -8,18 +8,20 @@ class IO
     # Load more common support
     require_relative 'common/ffi'
     require_relative 'common/timers'
+    require_relative 'common/poller'
 
     # Load platform-specific files
     if ::FFI::Platform::IS_BSD
       require_relative 'bsd/ffi'
-      require_relative 'bsd/poller'
+#      require_relative 'bsd/kqueue_poller'
+      require_relative 'common/select_poller'
     elsif ::FFI::Platform::IS_LINUX
       require_relative 'linux/ffi'
-      require_relative 'linux/poller'
+#      require_relative 'linux/epoll_poller'
+      require_relative 'common/select_poller'
     else
       # Can setup select(2) or poll(2) here as a backup for kqueue(2) and epoll(2)
-      #require_relative 'common/poller'
+      require_relative 'common/select_poller'
     end
-#    require_relative 'common/poller'
   end
 end
